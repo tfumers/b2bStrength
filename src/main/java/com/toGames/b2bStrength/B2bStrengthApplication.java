ce68e416ab39e2a4520b2d0dc7618f7076f0559d
Antonio <aefumero@hotmail.com>
2022-11-12 00:53:30 -0300

    first commit

---
+++ src/main/java/com/toGames/b2bStrength/B2bStrengthApplication.java
@@ -0,0 +1,13 @@
+package com.toGames.b2bStrength;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bStrengthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bStrengthApplication.class, args);
+	}
+
+}
